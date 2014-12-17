4b11ab8123798a2fe5f6f722d94584dbf5e56efd
Kazark <kazark@zoho.com>
2014-12-17 14:24:20 -0500

    Bootstrapped a Jasmine test suite

---
+++ GitHubUserStats.spec.js
@@ -0,0 +1,7 @@
+describe('GitHub User Stats', function() {
+    describe('Bootstrapping the project', function() {
+        it('should have running unit tests', function() {
+            expect(jasmine).toBeTruthy();
+        });
+    });
+});
